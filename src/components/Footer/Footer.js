974f40903d6f53c7d11f206daf32989054b53262
Jaan Lavaerts <jaan.lavaerts@student.ucll.be>
2023-06-27 18:58:35 +0200

    t

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,19 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
-    return (
-        <div>
-            <footer><span><Link to='/'>Game</Link></span><span id='footer'>&copy; Jaan Lavaerts </span><span id='leaderboard-footer'><Link to='/highscores'>Leaderboard</Link></span></footer>
-        </div>
-    )
-}
+  return (
+    <div>
+      <footer>
+        <span>
+          <Link to="/">Game</Link>
+        </span>
+        <span id="footer">&copy; Jaan Lavaertss </span>
+        {/* <span id='leaderboard-footer'><Link to='/highscores'>Leaderboard</Link></span> */}
+      </footer>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
